3f8481e9e3d06f4ecdb06c7aa827f9d9c7960f70
LeninCr <lenin.cordova.est@tecazuay.edu.ec>
2024-01-23 02:34:37 -0500

    Upload project

---
+++ src/services/ProductService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export class ProductService {
+    baseURL = "http://localhost:8080/api/product";
+
+    create(product) {
+        return axios.post(this.baseURL + "/create", product).then(res => res.data);
+    }
+
+    readAll() {
+        return axios.get(this.baseURL).then(res => res.data);
+    }
+    
+    update(product) {
+        return axios.put(this.baseURL + "/update/" + product._id, product).then(res => res.data);
+    }
+
+    delete(id) {
+        return axios.delete(this.baseURL + "/" + id).then(res => res.data);
+    }
+}
